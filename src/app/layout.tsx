f12abfd2a6492f81ce35e0051eefc037a704af88
Rishabh <awasthirishabh56@gmail.com>
2025-10-27 19:14:30 +0530

    changes in file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,18 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17682763341"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17682763341');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
